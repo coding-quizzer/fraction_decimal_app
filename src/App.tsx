05adb681bc543c4f323583f5248015764c8a8ac0
William Gadd <wgadd@sasktel.net>
2023-02-09 12:15:02 -0600

    Import github image and link to the github repository and add a header to the page

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import "./App.css";
 import DecimalContainer from "./components/DecimalContainer";
 import { Fraction } from "./helpers/fractionDecimalFunctions";
 import FractionInputBox from "./components/FractionInputBox";
+import Footer from "./components/Footer";
 
 function App() {
   const [fraction, setFraction] = useState<Fraction | null>(null);
 
   return (
     <div className="App">
+      <h1>Fraction Decimal Explorer</h1>
       <FractionInputBox
         onSubmit={(fraction: Fraction) => setFraction(fraction)}
       />
       {fraction && <DecimalContainer fraction={fraction as Fraction} />}
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import "./footer.scss";
+const Footer = () => (
+  <footer id="main-footer">
+    <a href="https://github.com/coding-quizzer/fraction_decimal_app">
+      <img src="github-mark.svg"></img>
+    </a>
+  </footer>
+);
+
+export default Footer;
