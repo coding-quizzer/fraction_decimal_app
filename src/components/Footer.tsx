299daa6c14850ddedb25f292d14c1a4d06b55a99
William Gadd <wgadd@sasktel.net>
2023-02-09 12:41:38 -0600

    Add tooltip for github link, to make it clear where it leads

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,41 @@
+import { useFloating, useHover, useInteractions } from "@floating-ui/react";
+import { useState } from "react";
 import "./footer.scss";
-const Footer = () => (
-  <footer id="main-footer">
-    <a href="https://github.com/coding-quizzer/fraction_decimal_app">
-      <img src="github-mark.svg"></img>
-    </a>
-  </footer>
-);
+const Footer = () => {
+  const [isOpen, setIsOpen] = useState(true);
+  const { x, y, strategy, refs, context } = useFloating({
+    open: isOpen,
+    onOpenChange: setIsOpen,
+  });
+
+  const hover = useHover(context);
+  const { getReferenceProps, getFloatingProps } = useInteractions([hover]);
+  return (
+    <footer id="main-footer">
+      {isOpen && (
+        <section
+          id="main-footer--popper"
+          ref={refs.setFloating}
+          style={{
+            position: strategy,
+            top: y ?? 0,
+            left: x ?? 0,
+            width: "max-content",
+          }}
+          {...getFloatingProps}
+        >
+          Visit GitHub Repository
+        </section>
+      )}
+      <a
+        href="https://github.com/coding-quizzer/fraction_decimal_app"
+        ref={refs.setReference}
+        {...getReferenceProps}
+      >
+        <img src="github-mark.svg"></img>
+      </a>
+    </footer>
+  );
+};
 
 export default Footer;
